bb495cdd2f0551406580485ea9aed839ab493183
Junior Gantin <nioperas06@gmail.com>
2020-03-08 16:46:23 +0100

    version 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='browserlog',
-    version='0.0.4',
+    version='0.0.5',
     package_dir={'': 'src'},
     description='Masonite Log Viewer',
     long_description=open("README.md", "r", encoding="utf-8").read(),
